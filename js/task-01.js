4a9137728e44d81cafa1078bdbdf0873c8b4477e
Ivan Rudenko <rudenko.ivan81@gmail.com>
2023-09-03 21:35:49 +0200

    save

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,18 @@
 const categoriesList = document.querySelector('#categories');
-
-
 const categoryItems = categoriesList.querySelectorAll('li.item');
 
-const numbers = categoryItems.length
 
+const numbers = categoryItems.length;
 console.log('Number of categories:', numbers);
+
 
 categoryItems.forEach(categoryItem => {
 
   const categoryTitle = categoryItem.querySelector('h2').textContent;
-  
-
   const categoryElements = categoryItem.querySelectorAll('li');
+  const numberOfElements = categoryElements.length;
   
  
   console.log(`Category: ${categoryTitle}`);
-  console.log(`Elements: ${categoryElements.length}`);
+  console.log(`Elements: ${numberOfElements}`);
 });
